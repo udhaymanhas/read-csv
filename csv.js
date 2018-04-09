32fdf43bb55eb3f5a004c311214ba6aeac65a308
udhaymanhas <udhay.manhas@gmail.com>
2018-04-09 21:24:44 +0530

    line by line read and convert to object

--- csv.js
+++ csv.js
@@ -1,11 +1,30 @@
+var fs = require("fs");
+var csv = require("fast-csv");
+
+var readCsv = () => {
+
+  var stream = fs.createReadStream("test.csv");
+
+  csv
+   .fromStream(stream, {headers : ["email", "division", "duration", "start", "end"]})
+   .on("data", function(data){
+       console.log('Process=====================>\n',data);
+   })
+   .on("end", function(){
+       console.log("done");
+   });
+}
 
 var getLost = (month) => {
+  readCsv();
   return month;
 }
 var getGained = (month) => {
+  readCsv();
   return month;
 }
 var getDivision = (month, division) => {
+  readCsv();
   return month;
 }
 
